6581082e42bc41574708ad471bd4c19743b664f0
Zero2Zero <41254360+ZeroTwoZero@users.noreply.github.com>
2020-06-23 15:54:46 +0530

    Update CreateTablePageJS.js
    changed logic to handle length from front end.

--- TestWebSite/Scripts/CreateTablePageJS.js
+++ TestWebSite/Scripts/CreateTablePageJS.js
@@ -1,4 +1,4 @@
-﻿$(function () {
+$(function () {
 	var serverName = localStorage.getItem("serverName");
 	var tableNameInput = $("#tableName");
 	var sqlTypes;
@@ -6,6 +6,7 @@
 	$("#column").hide();
 	$("#sqlerror").hide();
 	$("#sqlsuccess").hide();
+	var dummySQLTypes = ["VarChar", "nvarchar", "BigInt"];
 	if (!localStorage.getItem("winAuth")) {
 		console.log(localStorage.getItem("serverUName"));
 		console.log(localStorage.getItem("serverPwd"));
@@ -31,10 +32,28 @@
 				$('<option></option>').val(val).html(text)
 			);
 		});
+        //Should be removed used for testing purpose
+		$.each(dummySQLTypes, function (val, text) {
+			$("#sqlTypes" + columnInt).append(
+				$('<option></option>').val(val).html(text)
+			);
+		});
 	});
 
 	$(document).on("click", ".deleteButton", function (e) {
 		$(this).parent().remove();
+	});
+
+	$(document).on("change", ".selectOptions", function (e) {
+		var selectedOption = $(this).children("option:selected").text();
+		var parentDiv = $(this).parent();
+		var lengthDivIsPresent = parentDiv.find(".lengthDiv").length;
+		if ((selectedOption.includes("var") || selectedOption.includes("Var")) && !lengthDivIsPresent) {
+			parentDiv.append("<div class='lengthDiv'><label>Length</label><input type='text'></input></div>");
+		}
+		else if (!selectedOption.includes("var") && !selectedOption.includes("Var") && lengthDivIsPresent) {
+			parentDiv.find(".lengthDiv").remove();
+		}
 	});
 
 	$("#createTable").click(function () {
@@ -44,6 +63,7 @@
 		var flag = 0;
 		var columnNames = [];
 		var columnTypes = [];
+		var columnLength = [];
 		$("#column").hide();
 		tableNameInput.css({ "border": "1px solid gray" });
 		if (tableName.length <= 0) {
@@ -56,12 +76,24 @@
 		}
 		else {
 			$("#columnsDiv div").find("input").each(function () {
+				$(this).css({ "border": "1px solid gray" });
 				if (($.trim($(this).val()).length <= 0)) {
 					$(this).css({ "border": "1px solid red" });
 					flag = 1;
 				}
 			});
 		}
+		var lengthDiv = $("#columnsDiv").find(".lengthDiv");
+		if (lengthDiv.length) {
+			lengthDiv.each(function () {
+				var input = $(this).find("input");
+				input.css({ "border": "1px solid gray" });
+				if ($.trim(input.val()).length <= 0) {
+					input.css({ "border": "1px solid red" });
+					flag = 1;
+                }
+			});
+        }
 		if (flag == 1) {
 			return;
 		}
@@ -71,6 +103,10 @@
 		$("#columnsDiv div").find("select").each(function () {
 			columnTypes.push($.trim($(this).children("option:selected").text()));
 		});
+		$("#columnsDiv").find(".lengthDiv").each(function () {
+			columnLength.push($.trim($(this).find("input").val()));
+		});
+		console.log(columnLength);
 		var jsonParam = { TableName: tableName, ColumnNames: columnNames, ColumnTypes: columnTypes }
 		$.ajax({
 			url: "../Handlers/CreateTableInDB.ashx",
